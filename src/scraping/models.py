35eb92511649acf9bd2d8d9f2b67a6074ae51b7c
AkbarAli <88106359+satipoff@users.noreply.github.com>
2022-09-01 14:36:12 +0500

    [INFO]: (008) Modified

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -3,5 +3,12 @@ from django.db import models
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='Shaxar nomi')
     slug = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name = 'Shaxar nomi'
+        verbose_name_plural = 'Shaxarlar nomi'
+
+    def __str__(self):
+        return self.name
