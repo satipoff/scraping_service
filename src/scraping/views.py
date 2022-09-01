b97c3c7478fd2b5a41f95834460c687c8ce3d2e0
AkbarAli <88106359+satipoff@users.noreply.github.com>
2022-09-01 22:02:55 +0500

    [INFO]: (017) Modified 🚀

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
 import datetime
+from django.shortcuts import render
+from .models import Vacancy
 
 
 def home_page(request):
     date = datetime.datetime.now().date()
     user = 'Akbar'
 
-    _context = {'date': date, 'user': user}
+    qs = Vacancy.objects.all()
+
+    _context = {'date': date, 'user': user, 'object_list': qs}
 
     return render(request, 'scraping/home.html', _context)
