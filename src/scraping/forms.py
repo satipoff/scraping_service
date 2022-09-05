d1c6fac8b432df3e2cb8a3c9b3b639b3c8d77692
AkbarAli <88106359+satipoff@users.noreply.github.com>
2022-09-05 13:05:35 +0500

    [INFO]: (021/002) Modified 🚀

---
+++ src/scraping/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+from src.scraping.models import City, Language
+
+
+class FindForm(forms.Form):
+    city = forms.ModelChoiceField(queryset=City.objects.all())
+    language = forms.ModelChoiceField(queryset=Language.objects.all())
--- src/scraping/views.py
+++ src/scraping/views.py
@@ -7,7 +7,18 @@ def home_page(request):
     date = datetime.datetime.now().date()
     user = 'Akbar'
 
-    qs = Vacancy.objects.all()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__name'] = city
+        if language:
+            _filter['language__name'] = language
+
+        qs = Vacancy.objects.filter(**_filter)
 
     _context = {'date': date, 'user': user, 'object_list': qs}
 
